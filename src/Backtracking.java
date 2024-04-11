536c11779e10ee176532b2658b993cbc700b292d
Adarsh Singh <131537713+AdarshSingh9540@users.noreply.github.com>
2024-04-11 10:21:18 +0530

    Update Backtracking.java

--- src/Backtracking.java
+++ src/Backtracking.java
@@ -2,20 +2,32 @@ import java.util.*;
 
 class Main {
     public static void main(String[] args) {
-
-      System.out.println(count(4,4));
-      
-  }
-
-  static int count(int r, int c){
-    if(r== 1 || c == 1){
-      return 1;
+        System.out.println(count(3, 3));
+        path("", 3, 3); 
     }
 
-    int left = count(r-1, c);
-    int right = count(r, c-1);
+    static int count(int r, int c) {
+        if (r == 1 || c == 1) {
+            return 1;
+        }
 
-    return left + right;
-    
-  }
+        int left = count(r - 1, c);
+        int right = count(r, c - 1);
+
+        return left + right;
+    }
+
+    static void path(String p, int r, int c) {
+        if (r == 1 && c == 1) {
+            System.out.println(p);
+            return;
+        }
+
+        if (r > 1) {
+            path(p + "D", r - 1, c);
+        }
+        if (c > 1) {
+            path(p + "R", r, c - 1);
+        }
+    }
 }
