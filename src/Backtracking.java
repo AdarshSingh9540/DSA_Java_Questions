3657c7fb94f9265e634542412272b8e26d65228a
Adarsh Singh <131537713+AdarshSingh9540@users.noreply.github.com>
2024-04-11 13:26:27 +0530

    pathwithrestriction Backtracking.java

--- src/Backtracking.java
+++ src/Backtracking.java
@@ -48,4 +48,23 @@ class Main {
        diagonalpath(p+"H",r,c-1);
      }
    }
+
+
+    static void pathwithrestrictions(String p, boolean[][] maze, int r, int c) {
+          if (r == maze.length - 1 && c == maze[0].length - 1) {
+              System.out.println(p);
+              return;
+          }
+
+          if (!maze[r][c]) {
+              return;
+          }
+
+          if (r < maze.length - 1) {
+              pathwithrestrictions(p + "D", maze, r + 1, c);
+          }
+          if (c < maze[0].length - 1) {
+              pathwithrestrictions(p + "R", maze, r, c + 1);
+          }
+      }
 }
