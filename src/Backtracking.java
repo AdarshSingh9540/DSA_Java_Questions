617791f9c9d70a37d6bc423dea457aca8361eeea
Adarsh Singh <131537713+AdarshSingh9540@users.noreply.github.com>
2024-04-11 18:37:07 +0530

    Update all type of questions of Backtracking.java

--- src/Backtracking.java
+++ src/Backtracking.java
@@ -1,76 +1,61 @@
-import java.util.*;
+  import java.util.*;
 
-class Main {
-    public static void main(String[] args) {
-        System.out.println(count(3, 3));
-        path("", 3, 3); 
-    }
+  class Main {
+      public static void main(String[] args) {
+          boolean[][] board = {
+              {true, true, true},
+              {true, true, true},
+              {true, true, true}
+          };
 
-    static int count(int r, int c) {
-        if (r == 1 || c == 1) {
-            return 1;
-        }
+          // allpath("", board, 0, 0);
+        int[][] path = new int[board.length][board[0].length];
+        allpathprint("",board, 0, 0,1,path);
+      }
 
-        int left = count(r - 1, c);
-        int right = count(r, c - 1);
+      static int count(int r, int c) {
+          if (r == 1 || c == 1) {
+              return 1;
+          }
 
-        return left + right;
-    }
+          int left = count(r - 1, c);
+          int right = count(r, c - 1);
 
-    static void path(String p, int r, int c) {
-        if (r == 1 && c == 1) {
-            System.out.println(p);
-            return;
-        }
+          return left + right;
+      }
 
-        if (r > 1) {
-            path(p + "D", r - 1, c);
-        }
-        if (c > 1) {
-            path(p + "R", r, c - 1);
-        }
-    }
-
-
-     static void diagonalpath(String p, int r, int c){
-     if(r==1 && c==1){
-       System.out.println(p);
-       return;
-     }
-
-     if(r>1 && c>1){
-       diagonalpath(p+"D",r-1,c-1);
-     }
-     if(r>1){
-       diagonalpath(p+"V",r-1,c);
-     }
-     if(c>1){
-       diagonalpath(p+"H",r,c-1);
-     }
-   }
-
-
-    static void pathwithrestrictions(String p, boolean[][] maze, int r, int c) {
-          if (r == maze.length - 1 && c == maze[0].length - 1) {
+      static void path(String p, int r, int c) {
+          if (r == 1 && c == 1) {
               System.out.println(p);
               return;
           }
 
-          if (!maze[r][c]) {
-              return;
+          if (r > 1) {
+              path(p + "D", r - 1, c);
           }
-
-          if (r < maze.length - 1) {
-              pathwithrestrictions(p + "D", maze, r + 1, c);
-          }
-          if (c < maze[0].length - 1) {
-              pathwithrestrictions(p + "R", maze, r, c + 1);
+          if (c > 1) {
+              path(p + "R", r, c - 1);
           }
       }
 
+      static void diagonalpath(String p, int r, int c) {
+          if (r == 1 && c == 1) {
+              System.out.println(p);
+              return;
+          }
 
-    // ALl path
-     static void pathwithrestrictions(String p, boolean[][] maze, int r, int c) {
+          if (r > 1 && c > 1) {
+              diagonalpath(p + "D", r - 1, c - 1);
+          }
+          if (r > 1) {
+              diagonalpath(p + "V", r - 1, c);
+          }
+          if (c > 1) {
+              diagonalpath(p + "H", r, c - 1);
+          }
+      }
+
+      static void pathwithrestrictions(String p, boolean[][] maze, int r, int c) {
           if (r == maze.length - 1 && c == maze[0].length - 1) {
               System.out.println(p);
               return;
@@ -111,5 +96,39 @@ class Main {
         }
 
         maze[r][c] = true;
+     
     }
-}
+
+    static void allpathprint(String p, boolean[][] maze, int r, int c ,int step , int[][] path) {
+        if (r == maze.length - 1 && c == maze[0].length - 1) {
+          path[r][c] =step;
+           for(int[] arr :path){
+             System.out.println(Arrays.toString(arr));
+           }
+          
+          System.out.println(p);
+          System.out.println();
+            return;
+        }
+
+        if (!maze[r][c]) {
+            return;
+        }
+
+        maze[r][c] = false;
+         path[r][c] =step;
+
+        if (r < maze.length - 1) {
+              allpathprint(p + "D", maze, r + 1, c, step+1 ,  path);
+        }
+        if (c < maze[0].length - 1) {
+              allpathprint(p + "R", maze, r, c + 1, step+1 , path);
+        }
+        if (r > 0) { 
+          allpathprint(p + "U", maze, r - 1, c, step+1 ,  path); 
+        }
+
+        maze[r][c] = true;
+      path[r][c] =0;
+    }
+  }
